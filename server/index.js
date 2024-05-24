fece50b5b4488591a63f64d0b5705d3045d091f7
Rakesh <147958244+Rakesh7890v@users.noreply.github.com>
2024-05-24 21:21:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,16 +7,9 @@ const DoctorModel = require('./models/doctor')
 
 const app = express()
 
-const allowedOrigins = ['https://hospital-managments.vercel.app'];
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin:["https://hospital-managments.vercel.app"],
+    methods: ["POST", "GET", "PUT"],
     credentials: true
 }));
 
